90446e7cf68a9dc525d1e816687dc63a635b1e0f
Peter Kofler <peter.kofler@code-cop.org>
2015-02-18 00:00:41 +0100

    (red) defines BankOCR interface with failing guiding test.

---
+++ Java/src/main/java/kata/BankOcr.java
@@ -0,0 +1,15 @@
+package kata;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Example for the outside interface of the API we need to create.
+ */
+public class BankOcr {
+
+    public List<AccountNumber> parse(List<String> rawLines) throws IOException {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+}
---
+++ Java/src/test/java/kata/BankOcrTest.java
@@ -0,0 +1,28 @@
+package kata;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static kata.TestInput.allDigits;
+import static org.junit.Assert.assertEquals;
+
+public class BankOcrTest {
+
+    @Test
+    public void shouldParseOcrLine() throws IOException {
+        // guiding test from outside in as starting point.
+        // a) ignore this test
+        // b) or stub everything in this constructor
+        // c) or let it be red for most of the time
+        BankOcr bankOcr = new BankOcr();
+
+        List<AccountNumber> accountNumbers = bankOcr.parse(allDigits());
+
+        assertEquals(1, accountNumbers.size());
+        assertEquals(new AccountNumber("123456789"), accountNumbers.get(0));
+    }
+
+
+}
