d1e6652df278113295465185cb64d259b230f122
Peter Kofler <peter.kofler@code-cop.org>
2019-01-09 23:31:37 +0100

    Sample test to see xUnit API.

---
+++ Csharp/BankOcr-Kata.Tests/XUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using Org.Codecop.Hello;
+using Xunit;
+
+namespace Org.Codecop.Hello.Tests
+{
+    /**
+    * Test the xUnit infrastructure.
+    */
+    public class XUnitTest
+    {
+
+        [Fact]
+        public void ShouldAssertPlainXUnit()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
