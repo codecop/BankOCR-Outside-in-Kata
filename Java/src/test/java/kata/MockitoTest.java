0200f2e92f953367e80bcbe92b5d0c561ad60350
Peter Kofler <peter.kofler@code-cop.org>
2019-01-09 23:23:20 +0100

    Add a when clause to Mockito example code.

--- Java/src/test/java/kata/MockitoTest.java
+++ Java/src/test/java/kata/MockitoTest.java
@@ -1,23 +1,31 @@
 package kata;
 
-import org.junit.Test;
-
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
 
 /**
  * Test the infrastructure.
+ * 
+ * @see "https://site.mockito.org/"
  */
 public class MockitoTest {
+
+    interface Foo {
+        boolean parse(String value);
+    }
 
     @Test
     public void shouldAssertWithMockito() {
         // arrange
-        Runnable runner = mock(Runnable.class);
+        Foo foo = mock(Foo.class);
+        when(foo.parse("false")).thenReturn(false);
         // act
-        runner.run();
+        foo.parse("abc");
         // assert
-        verify(runner).run();
+        verify(foo).parse("abc");
     }
 
 }
