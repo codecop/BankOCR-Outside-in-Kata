507e53fcb169ffb78520d74c1f54e56535dff46b
Peter Kofler <peter.kofler@code-cop.org>
2019-01-09 23:54:15 +0100

    Add NUnit infrastructure as well.

---
+++ Csharp/BankOcr-Kata.Tests/NUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using Org.Codecop.Hello;
+using NUnit.Framework;
+
+namespace Org.Codecop.Hello.Tests
+{
+    /// <summary>
+    /// Test the NUnit infrastructure.
+    /// See https://github.com/nunit/docs/wiki/NUnit-Documentation
+    /// </summary>
+    [TestFixture]
+    public class NUnitTest
+    {
+
+        [Test]
+        public void ShouldAssertPlainNUnit()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+
+        [Test]
+        public void ShouldAssertWithNUnitMatchers() {
+            Assert.That(1 + 1, Is.EqualTo(2));
+        }
+    }
+}
