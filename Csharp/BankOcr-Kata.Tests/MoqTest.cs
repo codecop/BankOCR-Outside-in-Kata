867d1f8c6e743fbc074cb54477ff5e847fb84555
Peter Kofler <peter.kofler@code-cop.org>
2019-01-09 23:24:16 +0100

    Sample test to see Moq API.

---
+++ Csharp/BankOcr-Kata.Tests/MoqTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using Moq;
+
+namespace Kata.Tests
+{
+    public interface IFoo
+    {
+        bool Parse(string value);
+    }
+
+    /**
+     * Test the infrastructure.
+     * See https://github.com/Moq/moq4/wiki/Quickstart
+     */
+    public class MoqTest
+    {
+        [Fact]
+        public void shouldAssertWithMoq()
+        {
+            // arrange
+            var foo = new Mock<IFoo>();
+            foo.Setup(f => f.Parse("false")).Returns(false);
+            // act
+            foo.Object.Parse("abc");
+            // assert
+            foo.Verify(f => f.Parse("abc"));
+        }
+
+    }
+}
