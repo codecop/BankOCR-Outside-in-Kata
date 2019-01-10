cd68f1a762b87c2c5a8ba7e1cfae4615a0e6dae6
Peter Kofler <peter.kofler@code-cop.org>
2019-01-10 13:49:45 +0100

    Format documentation tests.

--- Python/test/test_mock.py
+++ Python/test/test_mock.py
@@ -1,10 +1,12 @@
 # coding=utf-8
 import unittest
+
 from mock import Mock
 
 
 class MockCase(unittest.TestCase):
     """Test the infrastructure."""
+
     # see https://docs.python.org/dev/library/unittest.mock.html
 
     # duck type Foo
@@ -17,10 +19,11 @@ class MockCase(unittest.TestCase):
         def stub(value):
             if value == "false":
                 return False
+
         foo.parse.side_effect = stub
 
         # act
-        foo.parse("abc");
+        foo.parse("abc")
 
         # assert
         foo.parse.assert_called_with("abc")
