5a848c18bdd3e7a62a828ac25c85387c83191db1
Peter Kofler <peter.kofler@code-cop.org>
2019-01-11 15:15:10 +0100

    Mocha and Sinon sample tests.

---
+++ JavaScript/test/mocha_test.js
@@ -0,0 +1,18 @@
+/* globals describe, it */
+
+const assert = require('assert');
+const chai = require('chai');
+const should = chai.should();
+
+describe('Mocha infrastructure', () => {
+
+    it('should assert plain Node', () => {
+        // Node.js’ built-in assert
+        assert.equal(2, 1 + 1);
+    });
+
+    it('should assert with Chai', () => {
+        (1 + 1).should.equal(2);
+    });
+
+});
