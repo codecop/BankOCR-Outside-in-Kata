3ae978f05de971cb341fbd63e21d9fb8bb803a28
Peter Kofler <peter.kofler@code-cop.org>
2019-12-13 10:52:26 +0100

    Test AccountNumber in Java.

---
+++ Java/src/test/java/kata/AccountNumberTest.java
@@ -0,0 +1,33 @@
+package kata;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class AccountNumberTest {
+
+    @Test
+    public void shouldBeEqualInList() {
+        List<AccountNumber> accountNumbers = Arrays.asList(new AccountNumber("123456789"));
+
+        assertEquals(1, accountNumbers.size());
+        assertEquals(new AccountNumber("123456789"), accountNumbers.get(0));
+    }
+
+    @Test
+    public void shouldNotBeEqual() {
+        assertNotEquals(new AccountNumber("123456789"), new AccountNumber("123456788"));
+    }
+
+    @Test
+    public void shouldListNumberAsString() {
+        AccountNumber accountNumber = new AccountNumber("123456789");
+
+        assertEquals("123456789", accountNumber.toString());
+    }
+
+}
