3265bcc9796cbe854169c6b33ec69daee6804911
Claus <claus.aichinger@mailbox.org>
2021-01-20 19:54:30 +0100

    clean up AccountNumber test cases

--- Python/test/test_accountnumber.py
+++ Python/test/test_accountnumber.py
@@ -5,24 +5,17 @@ from kata.accountnumber import AccountNumber
 
 
 class AccountNumberTestCase(unittest.TestCase):
-    def test_equal_in_list(self):
-        account_numbers = [AccountNumber("123456789")]
-
-        self.assertEqual(1, len(account_numbers))
-        self.assertEqual(AccountNumber("123456789"), account_numbers[0])
+    def test_equal(self):
+        self.assertEqual(AccountNumber("123456789"), AccountNumber("123456789"))
 
     def test_not_equal(self):
         self.assertNotEqual(AccountNumber("123456789"), AccountNumber("123456788"))
 
     def test_number_as_string(self):
-        account_number = AccountNumber("123456789")
-
-        self.assertEqual("123456789", str(account_number))
+        self.assertEqual("123456789", str(AccountNumber("123456789")))
 
     def test_repr(self):
-        account_number = AccountNumber("123456789")
-
-        self.assertEqual("AccountNumber('123456789')", repr(account_number))
+        self.assertEqual("AccountNumber('123456789')", repr(AccountNumber("123456789")))
 
     def test_validate_number_length(self):
         self.assertRaises(ValueError, lambda: AccountNumber("12345678"))
