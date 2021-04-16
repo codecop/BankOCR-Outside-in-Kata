976351d3ddb12b8464cf7292757ef870df7492c1
Peter Kofler <peter.kofler@code-cop.org>
2021-04-17 00:18:56 +0200

    Script to run with Gambit and Chicken.

---
+++ Scheme/run_tests_gambit.bat
@@ -0,0 +1,24 @@
+@set r5rs="C:\Program Files\Gambit\v4.8.1\bin\gsi.exe" -:s 
+@rem -standard 
+
+@color 02
+call %r5rs% test\scheme-unit-test.scm
+@if ERRORLEVEL 1 goto :fail
+
+call %r5rs% test\account-number-test.scm
+@if ERRORLEVEL 1 goto :fail
+
+call %r5rs% test\guiding_test.scm
+@if ERRORLEVEL 1 goto :fail
+
+call %r5rs% test\bank-ocr-test.scm
+@if ERRORLEVEL 1 goto :fail
+
+@echo test suite OK
+@goto :stop
+
+:fail
+@color 04
+@echo FAILED: !errorlevel!
+
+:stop
